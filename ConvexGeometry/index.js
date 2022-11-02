75a8698f9d69f5f354eaae1e1e92cd9864829ef1
christhopermarcelino <christhopermarcelino@gmail.com>
2022-11-02 13:16:14 +0700

    simplify convex geometry

--- ConvexGeometry/index.js
+++ ConvexGeometry/index.js
@@ -1,99 +1,38 @@
-function init() {
-  // use the defaults
-  var stats = initStats();
-  var renderer = initRenderer();
-  var camera = initCamera();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-  // create a scene, that will hold all our elements such as objects, cameras and lights.
-  // and add some simple default lights
-  var scene = new THREE.Scene();
-  initDefaultLighting(scene);
-  var groundPlane = addLargeGroundPlane(scene);
-  groundPlane.position.y = -30;
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-  var step = 0;
-  var spGroup;
-
-  // setup the control gui
-  var controls = new (function () {
-    this.appliedMaterial = applyMeshNormalMaterial;
-    this.castShadow = true;
-    this.groundPlaneVisible = true;
-
-    this.redraw = function () {
-      redrawGeometryAndUpdateUI(gui, scene, controls, function () {
-        return generatePoints();
-      });
-    };
-  })();
-
-  var gui = new dat.GUI();
-  gui
-    .add(controls, "appliedMaterial", {
-      meshNormal: applyMeshNormalMaterial,
-      meshStandard: applyMeshStandardMaterial,
-    })
-    .onChange(controls.redraw);
-
-  gui.add(controls, "redraw");
-  gui.add(controls, "castShadow").onChange(function (e) {
-    controls.mesh.castShadow = e;
-  });
-  gui.add(controls, "groundPlaneVisible").onChange(function (e) {
-    groundPlane.material.visible = e;
-  });
-
-  controls.redraw();
-  var step = 0;
-  render();
-
-  function generatePoints() {
-    if (spGroup) scene.remove(spGroup);
-    // add 10 random spheres
-    var points = [];
-    for (var i = 0; i < 10; i++) {
-      var randomX = -15 + Math.round(Math.random() * 30);
-      var randomY = -15 + Math.round(Math.random() * 30);
-      var randomZ = -15 + Math.round(Math.random() * 30);
-
-      points.push(new THREE.Vector3(randomX, randomY, randomZ));
-    }
-
-    spGroup = new THREE.Object3D();
-    var material = new THREE.MeshBasicMaterial({
-      color: 0xff0000,
-      transparent: false,
-    });
-    points.forEach(function (point) {
-      var spGeom = new THREE.SphereGeometry(0.2);
-      var spMesh = new THREE.Mesh(spGeom, material);
-      spMesh.position.copy(point);
-      spGroup.add(spMesh);
-    });
-    // add the points as a group to the scene
-    scene.add(spGroup);
-
-    // use the same points to create a convexgeometry
-    var convexGeometry = new THREE.ConvexGeometry(points);
-    convexGeometry.computeVertexNormals();
-    convexGeometry.computeFaceNormals();
-    convexGeometry.normalsNeedUpdate = true;
-    return convexGeometry;
-  }
-
-  function render() {
-    stats.update();
-    controls.mesh.rotation.y = step += 0.005;
-    controls.mesh.rotation.x = step;
-    controls.mesh.rotation.z = step;
-
-    if (spGroup) {
-      spGroup.rotation.y = step;
-      spGroup.rotation.x = step;
-      spGroup.rotation.z = step;
-    }
-
-    requestAnimationFrame(render);
-    renderer.render(scene, camera);
-  }
+const points = [];
+for (var i = 0; i < 10; i++) {
+  const randomX = -2.5 + Math.round(Math.random() * 5);
+  const randomY = -2.5 + Math.round(Math.random() * 5);
+  const randomZ = -2.5 + Math.round(Math.random() * 5);
+  points.push(new THREE.Vector3(randomX, randomY, randomZ));
 }
+console.log(points);
+const geometry = new THREE.ConvexGeometry(points);
+const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
